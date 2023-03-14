cfcb94bf92476445121111dc89574981c9f5ae0f
HENG-VISOTHI <visothibd@gmail.com>
2023-03-14 23:22:36 +0700

    'first-commit'

---
+++ IP-TP07/EX3/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+
+app.get('/', function (req, res) {
+  fs.readFile('./src/index.html/', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.send("There is a server error");
+    }
+    res.send(data);
+  });
+})
+app.listen(3000, () => {
+  console.log("The server is running on port 3000");
+})
